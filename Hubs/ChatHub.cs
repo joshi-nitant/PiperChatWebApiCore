bb6096a73eaa988266252932389ee0631a030572
Nitant Joshi <nitantjoshi20@gmail.com>
2021-04-02 02:52:41 +0530

    Initial Commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiperChatWebApiCore.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessageToServer(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+            Console.WriteLine("Recieved message" + message);
+        }
+    }
+}
