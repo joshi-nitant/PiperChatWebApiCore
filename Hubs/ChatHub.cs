bbb114dc94527d0bc52237e1d85d9fe2a39048ed
Nitant Joshi <nitantjoshi20@gmail.com>
2021-04-03 15:01:22 +0530

    Encryption with chat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,26 +1,52 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using PiperChatWebApiData.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PiperChatWebApiCore.Hubs
 {
     public class ChatHub:Hub
     {
-        Dictionary<int, string> onlineUsers = new Dictionary<int, string>();
-
-        public async Task UserConnected(string user)
+        static Dictionary<int, ChatUser> onlineUsers = new Dictionary<int, ChatUser>();
+        static Dictionary<int, string> connectionIdS = new Dictionary<int, string>();
+        public async Task UserConnected(string jsonString)
         {
+            Console.WriteLine("User connected"+jsonString);
+            await Clients.Caller.SendAsync("NewUserConnected", Newtonsoft.Json.JsonConvert.SerializeObject(onlineUsers));
+            ChatUser chatUser = JsonSerializer.Deserialize<ChatUser>(jsonString);
+            if (!onlineUsers.ContainsKey(chatUser.ChatUserId))
+            {
+                connectionIdS.Add(chatUser.ChatUserId, Context.ConnectionId);
+                onlineUsers.Add(chatUser.ChatUserId, chatUser);
+                await Clients.Others.SendAsync("NotifyToOthers", Newtonsoft.Json.JsonConvert.SerializeObject(chatUser));
+            }
             
-            Console.WriteLine(user);
-            await Clients.Others.SendAsync("NewUserConnected", user);
+            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(onlineUsers));
+            //Console.WriteLine(JsonSerializer.Serialize(onlineUsers));
+
+        }
+        public async Task UserDisconnected(string jsonString)
+        {
+            Console.WriteLine("User disconnceted" + jsonString);
+            ChatUser chatUser = JsonSerializer.Deserialize<ChatUser>(jsonString);
+            if (onlineUsers.ContainsKey(chatUser.ChatUserId))
+            {
+                connectionIdS.Remove(chatUser.ChatUserId);
+                onlineUsers.Remove(chatUser.ChatUserId);
+                await Clients.Others.SendAsync("NotifyToOthersDisconnect", Newtonsoft.Json.JsonConvert.SerializeObject(chatUser));
+            }
+
         }
 
-        public async Task SendMessageToServer(string message)
+        public async Task SendMessage(string jsonString)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
-            Console.WriteLine("Recieved message" + message);
+            Console.WriteLine("Recieved message" + jsonString);
+            Message message = Newtonsoft.Json.JsonConvert.DeserializeObject<Message>(jsonString);
+            string connectId = connectionIdS[message.ReceiverId];
+            await Clients.Client(connectId).SendAsync("ReceiveMessage", jsonString);
         }
     }
 }
